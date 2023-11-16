022e18fd4a96ac6198b2b5efd33468b7c6933c58
방수빈 <milk011922@gmail.com>
2023-11-16 14:18:17 +0900

    1984_중간 평균값 구하기

--- SWEA/D2/1984.py
+++ SWEA/D2/1984.py
@@ -1,10 +1,8 @@
 T=int(input())
 
 for t in range(1,T+1):
-  num=list(map(int,input().split()))
-  max_num=max(num)
-  min_num=min(num)
-  num.remove(max_num)
-  num.remove(min_num)
-  avg=round(sum(num)/len(num))
+  nums=list(map(int,input().split()))
+  nums.sort()
+  sum_nums=sum(nums[1:9])
+  avg=round(sum_nums/8)
   print(f'#{t} {avg}')
