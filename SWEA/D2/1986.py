b1f420992d633099112fcfc28c21c4ca6c971af2
방수빈 <milk011922@gmail.com>
2023-11-16 14:13:31 +0900

    1986_지그재그 숫자

--- SWEA/D2/1986.py
+++ SWEA/D2/1986.py
@@ -4,5 +4,10 @@ for test_case in range(1,T+1):
   N=int(input())
   result=0
 
+  for i in range(1,N+1):
+    if i%2==1:
+      result+=i
+    elif i%2==0:
+      result-=i
 
   print(f'#{test_case} {result}')
