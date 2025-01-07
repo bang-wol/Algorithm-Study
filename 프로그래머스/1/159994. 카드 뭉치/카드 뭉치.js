137ed143817b4b96458ef98aa61c85a487106136
bang-wol <102708198+bang-wol@users.noreply.github.com>
2025-01-07 20:57:34 +0900

    [level 1] Title: 카드 뭉치, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,16 @@
+function solution(cards1, cards2, goal) {
+        
+    while (goal.length > 0) {
+        const word = goal.shift();
+        
+        if(cards1[0] === word){
+            cards1.shift();
+        } else if (cards2[0] === word){
+            cards2.shift();
+        } else {
+            return 'No'
+        }
+    };
+    
+    return 'Yes';
+}
